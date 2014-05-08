1e56a26b1893c726714f925c0ae1a81e6cb7a318
Henry <henryquoctran@gmail.com>
2014-05-08 10:26:56 -0700

    initial commit

---
+++ project.r
@@ -0,0 +1,89 @@
+
+# Uniform Distribution
+uniformdistrib <- function(n)
+{
+    repetitions <- 100  # repeated 100 times
+    list        <- c()
+
+    for(i in 1:repetitions)
+    {
+        sample <- runif(n, min=0, max=100)   # (r)andom (unif)orm
+        sample_mean <- mean(sample)         # calculate the mean
+        list <- c(list, sample_mean)        # push mean to list
+    }
+
+    # output the histogram
+    pdf(paste("uniform", n, ".pdf", sep = ""))
+    hist(list, freq=TRUE)
+}
+
+uniformdistrib(5)
+uniformdistrib(30)
+uniformdistrib(100)
+
+# Weibull Distribution
+weibulldistrib <- function(n)
+{
+    repetitions <- 100  # repeated 100 times
+    list        <- c()
+
+    for(i in 1:repetitions)
+    {
+        sample <- rweibull(n, shape=2, scale=10)
+        sample_mean <- mean(sample)         # calculate the mean
+        list <- c(list, sample_mean)        # push mean to list
+    }
+
+    # output the histogram
+    pdf(paste("weibull", n, ".pdf", sep = ""))
+    hist(list, freq=TRUE)
+}
+
+weibulldistrib(5)
+weibulldistrib(30)
+weibulldistrib(100)
+
+# Standard Beta Distribution
+stdbetadistrib <- function(n)
+{
+    repetitions <- 100  # repeated 100 times
+    list        <- c()
+
+    for(i in 1:repetitions)
+    {
+        sample <- rbeta(n, shape1=2, shape2=3)
+        sample_mean <- mean(sample)         # calculate the mean
+        list <- c(list, sample_mean)        # push mean to list
+    }
+
+    # output the histogram
+    pdf(paste("standardbeta", n, ".pdf", sep = ""))
+    hist(list, freq=TRUE)
+}
+
+stdbetadistrib(5)
+stdbetadistrib(30)
+stdbetadistrib(100)
+
+
+# Log Normal Distribution
+lognormaldistrib <- function(n)
+{
+    repetitions <- 100  # repeated 100 times
+    list        <- c()
+
+    for(i in 1:repetitions)
+    {
+        sample <- rlnorm(n)
+        sample_mean <- mean(sample)         # calculate the mean
+        list <- c(list, sample_mean)        # push mean to list
+    }
+
+    # output the histogram
+    pdf(paste("lognormal", n, ".pdf", sep = ""))
+    hist(list, freq=TRUE)
+}
+
+lognormaldistrib(5)
+lognormaldistrib(30)
+lognormaldistrib(100)
---
+++ readme.md
@@ -0,0 +1,15 @@
+# MATH 161A Project
+
+* Uniform Distribution - min = 0, max = 100
+* Weibull Distribution - shape(alpha) = 2, scale(beta) = 10
+* Beta Distribution - alpha = 2, beta = 3
+* LogNormal Distribution - mean = 0, std deviation = 1
+
+# Generating PDF files
+I've already included the PDF files I've generated in this repo, but if you want to play around with the code and generate your own, you're free to.
+
+You will need this:
+http://www.r-project.org/
+
+It is a command-line interface, so you will need to run it in the terminal.
+To execute, run `Rscript project.r`
